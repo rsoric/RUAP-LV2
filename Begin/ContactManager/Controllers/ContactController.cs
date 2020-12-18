1f095f915a24152a2d6bebc74ae1b9407e44811c
rsoric <robert.soric@gmail.com>
2020-12-18 23:57:12 +0100

    Implemented adding contacts

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -13,9 +13,23 @@ namespace ContactManager.Controllers
     {
         private ContactRepository contactRepository;
 
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
         }
     }
 }
